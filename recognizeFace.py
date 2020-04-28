4f37d83a7c282ab84e136f75a37dd8ebb4c7f19a
wangzhilv <wangzhilv@jd.com>
2020-04-28 13:23:20 +0800

    人脸识别

--- recognizeFace.py
+++ recognizeFace.py
@@ -1,10 +1,17 @@
 import cv2,sys
 faceClassifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # the HAAR cascade file, which contains the machine learned data for face detection
-objImage=cv2.imread(sys.argv[1]) # use cv2 to load image file
+# 读取系统输入的图片
+# objImage=cv2.imread(sys.argv[1])
+# 直接读取文件
+objImage=cv2.imread("./test.jpg", 1)
+
 cvtImage=cv2.cvtColor(objImage,cv2.COLOR_BGR2GRAY) # convert the image to gray scale
-foundFaces=faceClassifier.detectMultiScale(cvtImage,scaleFactor=1.3,minNeighbors=9,minSize=(50,50),flags = cv2.cv.CV_HAAR_SCALE_IMAGE) # to detect faces
+foundFaces=faceClassifier.detectMultiScale(cvtImage,scaleFactor=1.3,minNeighbors=9,minSize=(50,50),flags = cv2.IMREAD_GRAYSCALE) # to detect faces
 print(" There are {} faces in the input image".format(len(foundFaces)))
 for (x,y,w,h) in foundFaces:# to iterate each faces founded
 	cv2.rectangle(objImage,(x,y),(x+w,y+h),(0,0,255),2)
 cv2.imshow("Facial Recognition Result, click anykey of keyboard to exit", objImage) #show the image
+# 后面的数字取值范围是[0,9]
+# 图片质量会虽数字变大而变小
+cv2.imwrite("./face.png",objImage,[int(cv2.IMWRITE_PNG_COMPRESSION),5])
 cv2.waitKey(0)
